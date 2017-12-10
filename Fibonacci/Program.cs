acfdeb1b24087d36015273b6b029444c14b858a3
Stefaan Samyn <stefaan.samyn@hogent.be>
2017-12-10 15:48:43 +0100

    Voeg oplossing 2 toe: met arrays

--- Fibonacci/Program.cs
+++ Fibonacci/Program.cs
@@ -7,10 +7,34 @@ namespace _02exFibonacci
         static void Main(string[] args)
         {
             Console.WriteLine("Rij van Fibonnaci");
-            Console.WriteLine("Geef het volgnummer op van het getal dat moet berekend worden");
-            int volgnr = int.Parse(Console.ReadLine());
-            Console.WriteLine("Het getal is {0}", BerekenFibonacciZonderArray(volgnr));
+            //Console.WriteLine("Geef het volgnummer op van het getal dat moet berekend worden");
+            //int volgnr = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Het getal is {0}", BerekenFibonacciZonderArray(volgnr));
+
+            const int AantalGetallen = 1000;
+            int[] _fibonacci = VulArrayMetFibonacciGetallen(AantalGetallen);
+
+            int volgnr = 0;
+            while (volgnr < 1 || volgnr > AantalGetallen)
+            {
+                Console.WriteLine("Geef het volgnummer (1.." + AantalGetallen + ") van het getal op dat moet berekend worden");
+                volgnr = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Het getal is {0}", _fibonacci[volgnr - 1]);
+
             Console.ReadLine();
+        }
+
+        private static int[] VulArrayMetFibonacciGetallen(int aantalGetallen)
+        {
+            int[] fibonacci = new int[aantalGetallen];
+            fibonacci[0] = 0;
+            fibonacci[1] = 1;
+            for (int i = 2; i < aantalGetallen; i++)
+            {
+                fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+            }
+            return fibonacci;
         }
 
         private static int BerekenFibonacciZonderArray(int volgNummer)
